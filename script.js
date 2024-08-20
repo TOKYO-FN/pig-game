9e812d9f03db1d92df9353b4da7cf3e04bb35945
TOKYO-FN <youssef.magdyii.12@gmail.com>
2024-08-20 14:31:29 +0300

    "Added variable declarations for selecting elements and set initial conditions"

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 'use strict';
 
+// Selecting Elemens
 const score0EL = document.getElementById('score--0');
 const score1EL = document.getElementById('score--1');
 const diceEL = document.querySelector('dice');
+const btnNew = document.querySelector('btn--new');
+const btnRoll = document.querySelector('btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
-console.log(diceEL);
-
+// Starting conditions
 score0EL.textContent = 0;
 score1EL.textContent = 0;
 diceEL.classList.add('hidden');
+
+// Rolling dice functionality
