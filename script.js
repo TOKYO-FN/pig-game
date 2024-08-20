84a3720a1a2b1e384beedccac19a194b7d66fba1
TOKYO-FN <youssef.magdyii.12@gmail.com>
2024-08-20 14:32:12 +0300

    "Updated selectors for dice, new, and roll buttons to use class selectors instead of tag names."

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 // Selecting Elemens
 const score0EL = document.getElementById('score--0');
 const score1EL = document.getElementById('score--1');
-const diceEL = document.querySelector('dice');
-const btnNew = document.querySelector('btn--new');
-const btnRoll = document.querySelector('btn--roll');
+const diceEL = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
