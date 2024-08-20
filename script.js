c1c888fdc11f7b788cd736448cca4619a5b39545
TOKYO-FN <youssef.magdyii.12@gmail.com>
2024-08-20 14:22:40 +0300

    "Added JavaScript code to select and manipulate HTML elements, and added a CSS class to hide elements."

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const score0EL = document.getElementById('score--0');
+const score1EL = document.getElementById('score--1');
+const diceEL = document.querySelector('dice');
+
+console.log(diceEL);
+
+score0EL.textContent = 0;
+score1EL.textContent = 0;
+diceEL.classList.add('hidden');
